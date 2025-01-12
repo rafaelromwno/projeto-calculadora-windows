f50f092be4fd52e6a9f99c7c3eba7966f2c7cc3d
Rafael Romano Silva <romanosilvarafael@gmail.com>
2025-01-12 13:41:09 -0300

    feat: lógica inicial
    exibição de números na tela

---
+++ js/scripts.js
@@ -0,0 +1,117 @@
+const previousOperationText = document.querySelector('#previous-operation');
+const currentOperationText = document.querySelector('#current-operation');
+const buttons = document.querySelectorAll('#buttons-container button');
+
+class Calculator 
+{
+    constructor(previousOperationText, currentOperationText)
+    {
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
+    }
+
+    // add digit to calculator screen
+
+    addDigit(digit)
+    {
+        // check if current operation already has a dot
+
+        if(digit === '.' && this.currentOperationText.innerText.includes('.'))
+        {
+            return;
+        }
+
+        this.currentOperation = digit;
+        this.updateScreen();
+    }
+
+    // process all calculator operations
+
+    processOperation(operation)
+    {
+        // get current and previous value
+
+        let operationValue;
+        const previous = +this.previousOperationText.innerText;
+        const current = +this.currentOperationText.innerText;
+
+        switch (operation) 
+        {
+            case '+':
+                operationValue = previous + current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+
+            case '-':
+                operationValue = previous - current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+
+            case '*':
+                operationValue = previous * current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+
+            case '/':
+                operationValue = previous / current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+        
+            default:
+                return;
+        }
+    }
+
+    // change values of the calculator screen
+
+    updateScreen(operationValue = null, operation = null, current = null, previous = null)
+    {
+        if(operationValue === null)
+        {
+            this.currentOperationText.innerText += this.currentOperation;
+        }
+        else
+        {
+            // check if value is zero, if it is just add current value
+
+            if(previous === 0)
+            {
+                operationValue = current;
+            }
+
+            // add current value to previous
+            this.previousOperationText.innerText = `${operationValue} ${operation}`;
+            this.currentOperationText.innerText = '';
+        }
+        
+
+    }
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
+buttons.forEach
+(
+    (button) => 
+    {
+        button.addEventListener
+        (
+            "click", (event) => 
+            {
+                const value = event.target.innerText;
+
+
+                if(+value >= 0 || value === '.')
+                {
+                    calc.addDigit(value);
+                }
+                else
+                {
+                    calc.processOperation(value);
+                }
+                
+            }            
+        )
+    }
+)
